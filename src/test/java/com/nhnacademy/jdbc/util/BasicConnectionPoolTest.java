c46e6d9a07b7c14437306e1b39fc95166497f87a
jalyice <jalyice@gmail.com>
2024-10-28 13:12:11 +0900

    1

--- src/test/java/com/nhnacademy/jdbc/util/BasicConnectionPoolTest.java
+++ src/test/java/com/nhnacademy/jdbc/util/BasicConnectionPoolTest.java
@@ -21,7 +21,7 @@ class BasicConnectionPoolTest {
     @BeforeAll
     static void setUp() {
         //todo#0 - jdbcUrl, username, password를 설정하세요
-        basicConnectionPool = new BasicConnectionPool(com.mysql.cj.jdbc.Driver.class.getName(),"","","",5);
+        basicConnectionPool = new BasicConnectionPool(com.mysql.cj.jdbc.Driver.class.getName(),"jdbc:mysql://133.186.241.167:3306/nhn_academy_51","nhn_academy_51","e/Efh[]8R!gw23n(",5);
     }
 
     @AfterAll
